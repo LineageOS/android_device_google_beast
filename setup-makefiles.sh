G12-Development/android_device_amlogic_a95xf1

beast: Initial Commit
Change-Id: I5bbc92935d81897929edf3daefb27a9e09691f60

setup-makefiles.sh
@@ -1,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=beast
+export DEVICE_COMMON=g12-common
+export VENDOR_COMMON=amlogic
+export VENDOR_DEVICE=google
+
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"

LineageOS/android_device_google_beast

setup-makefiles.sh
@@ -1,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=beast
+export DEVICE_COMMON=gx-common
+export VENDOR_COMMON=amlogic
+export VENDOR_DEVICE=google
+
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"