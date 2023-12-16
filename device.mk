177debcdbf90f1c9ebffde278ff494d8052d83ae
Bruno Martins <bgcngm@gmail.com>
2023-12-16 21:54:10 +0000

    beast: Properly build WLAN kernel module
    Change-Id: I1b02fd710c43ef66025d5d395728785c0ce3ddbe
    (cherry picked from commit 5c646a1db49fbcb6d6b8575c88b243c9cdaec6f0)

--- device.mk
+++ device.mk
@@ -21,10 +21,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc \
     $(LOCAL_PATH)/init-files/init.recovery.beast.rc:recovery/root/init.recovery.beast.rc
 
-## Kernel Modules
-PRODUCT_PACKAGES += \
-    8822bs
-
 ## Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
