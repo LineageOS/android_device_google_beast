72ef851ffd9397aaf1c14ec420e9b2e45563d83e
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-13 13:20:37 -0400

    beast: Build without TEE for now
    * Our TEE is just too old at the moment, get it booting,
      come back to this later.
    * Maybe we can use oneday's KM4 blobs once/if those issues
      get figured out.
    
    Change-Id: Icca74915140834adfa71368c85fedef0008b2da0

--- extract-files.py
+++ extract-files.py
@@ -19,12 +19,6 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
-    'vendor/bin/hw/android.hardware.security.keymint-service.amlogic': blob_fixup()
-        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V4-ndk.so')
-        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
-        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so'),
-    'vendor/etc/init/tee-supplicant.rc': blob_fixup()
-        .regex_replace('/vendor/lib/', '/vendor/lib/modules/')
 }  # fmt: skip
 
 module = ExtractUtilsModule(
